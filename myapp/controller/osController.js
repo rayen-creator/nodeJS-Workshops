dd0d9b77a9a871ff421e694c7ac04d0cf8175028
Rayen Oueslati <rayenoueslati60@gmail.com>
2023-02-10 22:43:28 +0100

    ✅ add new route

--- myapp/controller/osController.js
+++ myapp/controller/osController.js
@@ -1,6 +1,5 @@
 const os = require("os")
 
-
 exports.os_requirement=(req , res)=>{
   res.status(200).json({
     "hostname" : os.hostname(),
@@ -13,6 +12,12 @@ exports.cpuList=(req,res)=>{
     res.status(200).json({
         "cpu":os.cpus()
     });
+}
 
-    
+exports.cpubyid=(req,res)=>{
+    const id=req.params.id;
+    const cpu=os.cpus()[id];
+    res.status(200).json({
+        "cpu":cpu
+    });
 }
