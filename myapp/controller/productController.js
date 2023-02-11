040b8b36071b6552da220edfae329124fa586b6e
Rayen Oueslati <rayenoueslati60@gmail.com>
2023-02-11 14:48:41 +0100

    ✅ add new routes

--- myapp/controller/productController.js
+++ myapp/controller/productController.js
@@ -4,3 +4,45 @@ exports.productList=(req,res)=>{
 
     res.status(200).json(products);
 }
+
+exports.getproductbyid=(req,res)=>{
+    const product=products[req.params.id];
+    if(!product){
+        res.status(404).json({message:"Product not found"});
+    }
+    res.status(200).json(product);
+}
+exports.getTotalpricebyid=(req,res)=>{
+    const product=products[req.params.id];
+    const qt=req.params.qt;
+    if(!product){
+        res.status(404).json({message:"Product not found"});
+    }
+    res.status(200).json({
+        "id" : req.params.id,
+        "qt" : qt,
+        "unit_price" : product.price,
+        "totalprice":product.price*qt
+    });
+}
+
+exports.getProductInStockByQt=(req,res)=>{
+    const qt=req.params.qt;
+    var product;
+  for (let key in products){
+    for (let key1 in products[key]){ 
+        if(key1=="stock"){
+            if(products[key][key1]>=qt){
+            product=products[key];
+            }
+        }
+
+    }
+  }
+  if (!product){
+    res.status(404).json({message:"Product not found"});
+  }
+  res.status(200).json(product);
+
+}
+
--- myapp/routes/routes.js
+++ myapp/routes/routes.js
@@ -2,9 +2,14 @@ const oscontroller=require('../controller/osController');
 const productcontroller=require('../controller/productController');
 
 module.exports = (app) => {
+    //os routes
     app.get("/os",oscontroller.os_requirement);
     app.get("/os/cpus",oscontroller.cpuList);
     app.get("/os/cpus/:id",oscontroller.cpubyid)
-
+    //product routes
     app.get("/products",productcontroller.productList);
+    app.get("/products/:id",productcontroller.getproductbyid);
+    app.get("/products/instock/:qt",productcontroller.getProductInStockByQt);
+    app.get("/products/:id/:qt",productcontroller.getTotalpricebyid);
+
 };
