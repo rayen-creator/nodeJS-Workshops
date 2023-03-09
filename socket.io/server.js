10d943d102651452fb4565bec024c6e6da02f92e
Rayen Oueslati <rayenoueslati60@gmail.com>
2023-03-09 10:26:12 +0100

    🚀 socket.io workshop

---
+++ socket.io/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const path=require('path');
+const chatRouter=require('./routes/chat');
+
+app.set("views",path.join(__dirname,"views"));
+app.set("view engine","twig");
+
+app.use("/chat",chatRouter);
+
+const server = app.listen(3000, () => {
+    console.log('Server running on http://localhost:3000/chat');
+});
+const io=require('socket.io')(server);
+
+io.on("connection",(socket)=>{
+    console.log("New user connected");
+})
